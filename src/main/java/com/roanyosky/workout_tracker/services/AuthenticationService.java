d7bd75e788f69e28ca7f094b611407d112cb9466
Roan Yosky Timane <roanyosky@gmail.com>
2026-02-14 11:50:03 +0200

    Create AuthenticationService

---
+++ src/main/java/com/roanyosky/workout_tracker/services/AuthenticationService.java
@@ -0,0 +1,45 @@
+package com.roanyosky.workout_tracker.services;
+
+import com.roanyosky.workout_tracker.dtos.AuthenticationResponseDto;
+import com.roanyosky.workout_tracker.dtos.CreateUserDto;
+import com.roanyosky.workout_tracker.dtos.LoginDto;
+import com.roanyosky.workout_tracker.dtos.UserDto;
+import com.roanyosky.workout_tracker.entities.User;
+import com.roanyosky.workout_tracker.mappers.UserMapper;
+import com.roanyosky.workout_tracker.repositories.UserRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class AuthenticationService {
+    private final UserRepository userRepository;
+    private final UserMapper userMapper;
+    private final JwtService jwtService;
+    private final PasswordEncoder passwordEncoder;
+
+    public AuthenticationResponseDto register(CreateUserDto createUserDto){
+        User user = userMapper.toEntity(createUserDto);
+        userRepository.save(user);
+
+        String jwtToken = jwtService.generateToken(user);
+        UserDto userDto = userMapper.toDto(user);
+        return AuthenticationResponseDto.builder()
+                .token(jwtToken)
+                .userDto(userDto)
+                .build();
+    }
+
+    public AuthenticationResponseDto authenticate(LoginDto loginDto){
+        User user = userRepository.findByEmail(loginDto.getEmail());
+
+        String jwtToken = jwtService.generateToken(user);
+        UserDto userDto = userMapper.toDto(user);
+
+        return AuthenticationResponseDto.builder()
+                .token(jwtToken)
+                .userDto(userDto)
+                .build();
+    }
+}
