2489afbaffe5f13b9846bf5d4df83c5339a22819
Roan Yosky Timane <roanyosky@gmail.com>
2026-02-14 11:45:09 +0200

    Create repository for the entity User

---
+++ src/main/java/com/roanyosky/workout_tracker/repositories/UserRepository.java
@@ -0,0 +1,11 @@
+package com.roanyosky.workout_tracker.repositories;
+
+import com.roanyosky.workout_tracker.entities.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.security.core.userdetails.UserDetails;
+
+import java.util.UUID;
+
+public interface UserRepository extends JpaRepository<User, UUID> {
+    User findByEmail(String email);
+}
