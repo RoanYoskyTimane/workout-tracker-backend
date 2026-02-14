d437c19677c2102c735906a543827bbd31f7619c
Roan Yosky Timane <roanyosky@gmail.com>
2026-02-14 11:52:17 +0200

    Create UserController

---
+++ src/main/java/com/roanyosky/workout_tracker/controllers/UserController.java
@@ -0,0 +1,32 @@
+package com.roanyosky.workout_tracker.controllers;
+
+import com.roanyosky.workout_tracker.dtos.AuthenticationResponseDto;
+import com.roanyosky.workout_tracker.dtos.CreateUserDto;
+import com.roanyosky.workout_tracker.dtos.LoginDto;
+import com.roanyosky.workout_tracker.services.AuthenticationService;
+import lombok.AllArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@AllArgsConstructor
+@RequestMapping("/api/v1/auth")
+public class UserController {
+    AuthenticationService authenticationService;
+
+    @PostMapping("/register")
+    public ResponseEntity<AuthenticationResponseDto> registerUser(@RequestBody CreateUserDto createUserDto){
+        AuthenticationResponseDto authenticationResponseDto = authenticationService.register(createUserDto);
+        return ResponseEntity.ok(authenticationResponseDto);
+    }
+
+    @PostMapping("/login")
+    public ResponseEntity<AuthenticationResponseDto> login(@RequestBody LoginDto loginDto){
+        AuthenticationResponseDto authenticationResponseDto = authenticationService.authenticate(loginDto);
+        return ResponseEntity.ok(authenticationResponseDto);
+    }
+}
