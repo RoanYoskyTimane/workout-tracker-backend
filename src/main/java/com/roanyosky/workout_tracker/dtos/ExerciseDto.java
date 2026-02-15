122ce9e5bf4f80345074e0bc7ed75e5989dcc536
Roan Yosky Timane <roanyosky@gmail.com>
2026-02-15 18:50:47 +0200

    Create the exercise dto's(create, update, response)

---
+++ src/main/java/com/roanyosky/workout_tracker/dtos/ExerciseDto.java
@@ -0,0 +1,15 @@
+package com.roanyosky.workout_tracker.dtos;
+
+import com.roanyosky.workout_tracker.entities.enums.CategoryType;
+import com.roanyosky.workout_tracker.entities.enums.MuscleGroupType;
+import lombok.Data;
+
+import java.util.UUID;
+
+@Data
+public class ExerciseDto {
+    private UUID id;
+    private String name;
+    private CategoryType category;
+    private MuscleGroupType muscleGroup;
+}
---
+++ src/main/java/com/roanyosky/workout_tracker/dtos/UpdateExerciseDto.java
@@ -0,0 +1,21 @@
+package com.roanyosky.workout_tracker.dtos;
+
+import com.roanyosky.workout_tracker.entities.enums.CategoryType;
+import com.roanyosky.workout_tracker.entities.enums.MuscleGroupType;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Pattern;
+import jakarta.validation.constraints.Size;
+import lombok.Data;
+
+@Data
+public class UpdateExerciseDto {
+    @NotNull
+    @Size(min = 2, max=100, message = "Name must be between 5 and 100 characters")
+    private String name;
+
+    @NotNull(message = "Category is required")
+    private CategoryType category;
+
+    @NotNull(message = "Muscle Group is required")
+    private MuscleGroupType muscleGroup;
+}
