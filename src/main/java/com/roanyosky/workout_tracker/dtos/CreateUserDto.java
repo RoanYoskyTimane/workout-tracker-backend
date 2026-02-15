00b38a3541582a2e5f8ee71315c1e93257bb88a1
Roan Yosky Timane <roanyosky@gmail.com>
2026-02-15 17:54:53 +0200

    Add validation to the update and create dto's for the user and the login dto

--- src/main/java/com/roanyosky/workout_tracker/dtos/CreateUserDto.java
+++ src/main/java/com/roanyosky/workout_tracker/dtos/CreateUserDto.java
@@ -1,9 +1,17 @@
 package com.roanyosky.workout_tracker.dtos;
 
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Size;
 import lombok.Data;
 
 @Data
 public class CreateUserDto {
+    @Email(message="Email cannot be null")
+    @NotNull
     private String email;
+
+    @NotNull
+    @Size(min = 8, max = 255, message = "Password must be between 8 and 255 characters.")
     private String password;
 }
--- src/main/java/com/roanyosky/workout_tracker/dtos/UpdateUserDto.java
+++ src/main/java/com/roanyosky/workout_tracker/dtos/UpdateUserDto.java
@@ -1,8 +1,12 @@
 package com.roanyosky.workout_tracker.dtos;
 
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Size;
 import lombok.Data;
 
 @Data
 public class UpdateUserDto {
+    @NotNull
+    @Size(min = 8, max = 255, message = "Password must be between 8 and 255 characters.")
     private String password;
 }
