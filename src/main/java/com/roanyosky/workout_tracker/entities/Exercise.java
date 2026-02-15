55a26fea345d41f0076774ce861e3e8788276893
Roan Yosky Timane <roanyosky@gmail.com>
2026-02-15 18:49:45 +0200

    Create Exercise Entity

---
+++ src/main/java/com/roanyosky/workout_tracker/entities/Exercise.java
@@ -0,0 +1,41 @@
+package com.roanyosky.workout_tracker.entities;
+
+import com.roanyosky.workout_tracker.entities.enums.CategoryType;
+import com.roanyosky.workout_tracker.entities.enums.MuscleGroupType;
+import jakarta.persistence.*;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Size;
+import lombok.Getter;
+import lombok.Setter;
+
+import java.util.UUID;
+
+@Getter
+@Setter
+@Entity
+@Table(name = "exercises")
+public class Exercise {
+    @Id
+    @GeneratedValue(strategy = GenerationType.UUID)
+    @Column(name = "id", nullable = false)
+    private UUID id;
+
+    @Size(max = 100)
+    @NotNull
+    @Column(name = "name", nullable = false, length = 100)
+    private String name;
+
+    @Size(max = 255)
+    @Column(name = "description")
+    private String description;
+
+    @Size(max = 50)
+    @Column(name = "category", length = 50)
+    @Enumerated(EnumType.STRING)
+    private CategoryType category;
+
+    @Size(max = 50)
+    @Column(name = "muscle_group", length = 50)
+    @Enumerated(EnumType.STRING)
+    private MuscleGroupType muscleGroup;
+}
