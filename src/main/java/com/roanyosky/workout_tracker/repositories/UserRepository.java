bfc3ca4a10ed5bf8ddc504ab1dc4131a2229e703
Roan Yosky Timane <roanyosky@gmail.com>
2026-02-16 22:32:54 +0200

    Add the repository tag to the UserRepository

--- src/main/java/com/roanyosky/workout_tracker/repositories/UserRepository.java
+++ src/main/java/com/roanyosky/workout_tracker/repositories/UserRepository.java
@@ -2,10 +2,11 @@ package com.roanyosky.workout_tracker.repositories;
 
 import com.roanyosky.workout_tracker.entities.User;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.stereotype.Repository;
 
 import java.util.UUID;
 
+@Repository
 public interface UserRepository extends JpaRepository<User, UUID> {
     User findByEmail(String email);
 }
