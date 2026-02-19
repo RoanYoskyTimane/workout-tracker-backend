4a3f4c9adaf8dd10960097fdba91d62921498d9c
Roan Yosky Timane <roanyosky@gmail.com>
2026-02-19 16:42:27 +0200

    Create entity Workout

---
+++ src/main/java/com/roanyosky/workout_tracker/entities/Workout.java
@@ -0,0 +1,45 @@
+package com.roanyosky.workout_tracker.entities;
+
+import com.roanyosky.workout_tracker.entities.enums.Status;
+import jakarta.persistence.*;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Size;
+import lombok.Getter;
+import lombok.Setter;
+import org.hibernate.annotations.ColumnDefault;
+import org.hibernate.annotations.OnDelete;
+import org.hibernate.annotations.OnDeleteAction;
+
+import java.time.Instant;
+import java.util.UUID;
+
+@Getter
+@Setter
+@Entity
+@Table(name = "workouts")
+public class Workout {
+    @Id
+    @GeneratedValue(strategy = GenerationType.UUID)
+    @Column(name = "id", nullable = false)
+    private UUID id;
+
+    @NotNull
+    @ManyToOne(fetch = FetchType.LAZY, optional = false)
+    @OnDelete(action = OnDeleteAction.CASCADE)
+    @JoinColumn(name = "user_id", nullable = false)
+    private User user;
+
+    @Size(max = 255)
+    @NotNull
+    @Column(name = "name", nullable = false)
+    private String name;
+
+    @NotNull
+    @Column(name = "scheduled_date", nullable = false)
+    private Instant scheduledDate = Instant.now();
+
+    @ColumnDefault("'pending'")
+    @Enumerated(EnumType.STRING)
+    @Column(name = "status", length = 50)
+    private Status status;
+}
